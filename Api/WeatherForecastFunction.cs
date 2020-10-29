86ceeae263f08b06aec5bfda4334562b2a38e094
peteskelly@gmail.com <Pete Skelly>
2020-10-28 20:45:23 -0400

    Minor auth changes

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Encodings;
+using System.Text.Json;
+using System.Security.Claims;
+using System.Text.Json.Serialization;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions;
 using Microsoft.Extensions.Logging;
-
 using BlazorApp.Shared;
 
 namespace BlazorApp.Api
@@ -33,10 +39,29 @@ namespace BlazorApp.Api
         }
 
         [FunctionName("WeatherForecast")]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
         {
+
+            
+            // var header = req.Headers["x-ms-client-principal"];
+            // var data = header.Value[0];
+            // var decoded = Convert.FromBase64String(data);
+            // var json = System.Text.ASCIIEncoding.ASCII.GetString(decoded);
+            // var principal = JsonSerializer.Deserialize<ClientPrincipal>(json);
+
+            //  principal.UserRoles = principal.UserRoles.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+  
+            // if (!principal.UserRoles.Any())
+            // {
+            //     //return new ClaimsPrincipal();
+            // }
+    
+            // var identity = new ClaimsIdentity(principal.IdentityProvider);
+            // identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            // identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            // identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+    
+
             var randomNumber = new Random();
             var temp = 0;
 
